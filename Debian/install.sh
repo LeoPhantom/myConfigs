e9145be4931ccfc8051319b8716eae34d26c4c26
Silver Phantom <michael.sheinker23@gmail.com>
2025-11-19 19:27:28 -0500

    Install script added

--- Debian/install.sh
+++ Debian/install.sh
@@ -1,6 +1,54 @@
 #!/bin/bash
 
 
-echo " Install Devian config for i3 "
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCEP[0]}")" && pwd)"
+CONFIG_DIR="$SCRIPT_DIR"/config
+PARANT_DIR="$(cd "$(dirname "${BASH_SOURCEP[0]}")" && cd ..  && pwd)"
+echo -e "=========Install script=========== \n"
+echo " Install Debian config for i3 "
 
-sudo apt install -y picom feh nigrogen alacritty 
+sudo apt install -y picom feh nigrogen alacritty fastfetch 
+
+
+echo -e  "Packeges installed \n"
+echo -e  "Making directorys \n"
+
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/picom
+mkdir -p ~/Pictures
+
+echo -e "Copying config files\n"
+sleep 3
+
+if [ -e ~/.config/i3/config ]; then
+    echo -e  "File exists.\n"
+    echo -e "Removing i3 config fil and replacing \n"
+    rm ~/.config/i3/config
+    cp -r configs/i3/config ~/.config/i3/
+
+ else 
+	 cp configs/i3/config ~/.config/i3/
+fi
+
+cp configs/alacritty/alacritty.toml  ~/.config/alacritty/
+cp -r configs/alacritty/themes  ~/.config/alacritty/
+cp  ../Pictures/*  ~/Pictures
+
+echo "The script for i3lock-colors is in config/scripts , manually paset it if u will use i3lock-color"
+sleep 3 
+
+echo -e "Restarting I3 !!"
+
+run=true
+count=3
+while [ $run = true ]; do
+	echo " in $count ..."
+	sleep 1
+	if [ $count -eq 0 ]; then
+		run=false
+	fi
+	count=$((count-1))
+done
+
+i3-msg restart
